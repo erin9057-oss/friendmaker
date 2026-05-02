001791df3274b1a1174145658639548fcd9d7cec
erin9057-oss <erin9057@gmail.com>
2026-05-03 01:02:45 +0800

    Update usb_controller_transport.cpp

--- firmware/esp32/src/usb_controller_transport.cpp
+++ firmware/esp32/src/usb_controller_transport.cpp
@@ -1,89 +1,145 @@
 #include "usb_controller_transport.h"
 #include "config.h"
+#include "USB.h"
+#include "USBHID.h"
 
-// 构造函数
-UsbControllerTransport::UsbControllerTransport() : gamepad_() {}
+// 1. 像素级复刻：Switch 官方授权 HORI Pokken 手柄的数据结构描述符
+static const uint8_t switch_report_descriptor[] = {
+    0x05, 0x01, 0x09, 0x05, 0xA1, 0x01, 0x15, 0x00,
+    0x25, 0x01, 0x35, 0x00, 0x45, 0x01, 0x75, 0x01,
+    0x95, 0x10, 0x05, 0x09, 0x19, 0x01, 0x29, 0x10,
+    0x81, 0x02, 0x05, 0x01, 0x25, 0x07, 0x46, 0x3B,
+    0x01, 0x75, 0x04, 0x95, 0x01, 0x65, 0x14, 0x09,
+    0x39, 0x81, 0x42, 0x65, 0x00, 0x95, 0x01, 0x81,
+    0x01, 0x26, 0xFF, 0x00, 0x46, 0xFF, 0x00, 0x09,
+    0x30, 0x09, 0x31, 0x09, 0x32, 0x09, 0x35, 0x75,
+    0x08, 0x95, 0x04, 0x81, 0x02, 0xC0
+};
+
+// 2. 匹配描述符的数据包结构（刚好 7 个字节）
+struct SwitchReport {
+    uint16_t buttons;
+    uint8_t hat;
+    uint8_t lx;
+    uint8_t ly;
+    uint8_t rx;
+    uint8_t ry;
+} __attribute__((packed));
+
+// 3. 我们自己手搓的纯血 Switch 专用底层 USB 驱动
+class SwitchUSBGamepad : public USBHIDDevice {
+private:
+    USBHID hid;
+    SwitchReport report;
+public:
+    SwitchUSBGamepad() {
+        static bool initialized = false;
+        if(!initialized){
+            hid.addDevice(this, sizeof(switch_report_descriptor));
+            initialized = true;
+        }
+        resetReport();
+    }
+    void begin() { hid.begin(); }
+    uint16_t _onGetDescriptor(uint8_t* buffer) override {
+        memcpy(buffer, switch_report_descriptor, sizeof(switch_report_descriptor));
+        return sizeof(switch_report_descriptor);
+    }
+    void resetReport() {
+        report.buttons = 0;
+        report.hat = 8; // 8 代表十字键居中无操作
+        report.lx = 128; // 128 代表摇杆居中
+        report.ly = 128;
+        report.rx = 128;
+        report.ry = 128;
+    }
+    void setButton(uint16_t bitmask) { report.buttons |= bitmask; }
+    void setHat(uint8_t hat) { report.hat = hat; }
+    void setAxes(uint8_t lx, uint8_t ly) { report.lx = lx; report.ly = ly; }
+    void sendReport() {
+        hid.SendReport(0, &report, sizeof(SwitchReport));
+    }
+};
+
+// 实例化我们的终极武器
+SwitchUSBGamepad switchPad;
+
+UsbControllerTransport::UsbControllerTransport() {}
 
 void UsbControllerTransport::begin() {
-  // 在启动 USB 前，强制把自己的身份证改成 Switch 官方授权的 HORI Pokken 手柄
+  // 贴上 HORI Pokken 的假身份证
   USB.VID(0x0F0D);
   USB.PID(0x0092);
   USB.usbClass(0);
   USB.usbSubClass(0);
   USB.usbProtocol(0);
 
-  gamepad_.begin();
+  switchPad.begin();
   USB.begin(); 
 }
 
 void UsbControllerTransport::applyButtonsToGamepad(uint32_t buttonsMask) {
-  // 定义标准按键数组，用于统一松开
-  uint8_t standardButtons[] = {
-    BUTTON_A, BUTTON_B, BUTTON_X, BUTTON_Y, 
-    BUTTON_TL, BUTTON_TR, BUTTON_TL2, BUTTON_TR2, 
-    BUTTON_START, BUTTON_SELECT, BUTTON_MODE
-  };
+  switchPad.resetReport();
   
-  // 先松开所有常规按键
-  for (uint8_t btn : standardButtons) {
-    gamepad_.releaseButton(btn);
-  }
+  // 将业务逻辑的按键精准翻译成 Switch 芯片能懂的二进制位
+  uint16_t b = 0;
+  if (buttonsMask & controllerButtonMask(ControllerButton::Y)) b |= 0x01;
+  if (buttonsMask & controllerButtonMask(ControllerButton::B)) b |= 0x02;
+  if (buttonsMask & controllerButtonMask(ControllerButton::A)) b |= 0x04;
+  if (buttonsMask & controllerButtonMask(ControllerButton::X)) b |= 0x08;
+  if (buttonsMask & controllerButtonMask(ControllerButton::L)) b |= 0x10;
+  if (buttonsMask & controllerButtonMask(ControllerButton::R)) b |= 0x20;
+  if (buttonsMask & controllerButtonMask(ControllerButton::ZL)) b |= 0x40;
+  if (buttonsMask & controllerButtonMask(ControllerButton::ZR)) b |= 0x80;
+  if (buttonsMask & controllerButtonMask(ControllerButton::Minus)) b |= 0x100;
+  if (buttonsMask & controllerButtonMask(ControllerButton::Plus)) b |= 0x200;
+  if (buttonsMask & controllerButtonMask(ControllerButton::Home)) b |= 0x1000;
+  switchPad.setButton(b);
 
-  // 根据掩码映射并按下对应的按键 (适配 ESP32 的命名规范)
-  if (buttonsMask & controllerButtonMask(ControllerButton::A)) gamepad_.pressButton(BUTTON_A);
-  if (buttonsMask & controllerButtonMask(ControllerButton::B)) gamepad_.pressButton(BUTTON_B);
-  if (buttonsMask & controllerButtonMask(ControllerButton::X)) gamepad_.pressButton(BUTTON_X);
-  if (buttonsMask & controllerButtonMask(ControllerButton::Y)) gamepad_.pressButton(BUTTON_Y);
-  if (buttonsMask & controllerButtonMask(ControllerButton::L)) gamepad_.pressButton(BUTTON_TL);
-  if (buttonsMask & controllerButtonMask(ControllerButton::R)) gamepad_.pressButton(BUTTON_TR);
-  if (buttonsMask & controllerButtonMask(ControllerButton::ZL)) gamepad_.pressButton(BUTTON_TL2);
-  if (buttonsMask & controllerButtonMask(ControllerButton::ZR)) gamepad_.pressButton(BUTTON_TR2);
-  
-  if (buttonsMask & controllerButtonMask(ControllerButton::Plus)) gamepad_.pressButton(BUTTON_START);
-  if (buttonsMask & controllerButtonMask(ControllerButton::Minus)) gamepad_.pressButton(BUTTON_SELECT);
-  if (buttonsMask & controllerButtonMask(ControllerButton::Home)) gamepad_.pressButton(BUTTON_MODE); 
-  
-  // 映射 D-Pad
-  if (buttonsMask & controllerButtonMask(ControllerButton::DpadUp)) gamepad_.hat(HAT_UP);
-  else if (buttonsMask & controllerButtonMask(ControllerButton::DpadDown)) gamepad_.hat(HAT_DOWN);
-  else if (buttonsMask & controllerButtonMask(ControllerButton::DpadLeft)) gamepad_.hat(HAT_LEFT);
-  else if (buttonsMask & controllerButtonMask(ControllerButton::DpadRight)) gamepad_.hat(HAT_RIGHT);
-  else gamepad_.hat(HAT_CENTER);
+  uint8_t hat = 8;
+  if (buttonsMask & controllerButtonMask(ControllerButton::DpadUp)) hat = 0;
+  else if (buttonsMask & controllerButtonMask(ControllerButton::DpadDown)) hat = 4;
+  else if (buttonsMask & controllerButtonMask(ControllerButton::DpadLeft)) hat = 6;
+  else if (buttonsMask & controllerButtonMask(ControllerButton::DpadRight)) hat = 2;
+  switchPad.setHat(hat);
 }
 
 void UsbControllerTransport::pressButtons(
     uint32_t buttonsMask, uint16_t holdMs, uint16_t settleMs) {
-  
   applyButtonsToGamepad(buttonsMask);
+  switchPad.sendReport();
   delay(holdMs);
   
-  // 传 0 进去，代表清空所有按键
-  applyButtonsToGamepad(0); 
-  // 恢复左摇杆居中
-  gamepad_.leftStick(0, 0);
-  
+  switchPad.resetReport();
+  switchPad.sendReport();
   delay(settleMs);
 }
 
 void UsbControllerTransport::moveDirection(
     int x, int y, uint16_t holdMs, uint16_t settleMs) {
   
-  // USBHIDGamepad 期望摇杆值在 -127 到 127 之间
-  int mappedX = x * 127;
-  int mappedY = y * 127;
-
-  // 使用 leftStick 接口
-  gamepad_.leftStick(mappedX, mappedY);
+  switchPad.resetReport();
+  
+  uint8_t lx = 128;
+  uint8_t ly = 128;
+  
+  // 坐标系转换：-1, 0, 1 转为 Switch 的 0, 128, 255
+  if (x < 0) lx = 0;
+  else if (x > 0) lx = 255;
+  
+  if (y < 0) ly = 0; 
+  else if (y > 0) ly = 255;
+  
+  switchPad.setAxes(lx, ly);
+  switchPad.sendReport();
   delay(holdMs);
   
-  // 恢复居中
-  gamepad_.leftStick(0, 0);
+  switchPad.resetReport();
+  switchPad.sendReport();
   delay(settleMs);
 }
 
-bool UsbControllerTransport::resetConnection() {
-  return true; 
-}
+bool UsbControllerTransport::resetConnection() { return true; }
 
 void UsbControllerTransport::printStatus(Print &output) const {
   output.print("INFO transport=");
